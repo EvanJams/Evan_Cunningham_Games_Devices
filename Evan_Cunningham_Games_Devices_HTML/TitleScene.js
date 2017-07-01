d6923af8746066857bac0367481af7b3ee322be6
evanjams <holy.falcon@yahoo.co.uk>
2017-07-01 20:54:37 +0100

    Javascript project Skeleton

---
+++ Evan_Cunningham_Games_Devices_HTML/TitleScene.js
@@ -0,0 +1,43 @@
+function TitleScene(){
+	this.title = "The Game";
+	this.playImg = new Image();
+	this.titleImg = new Image();
+	this.playImg.src = 'resources/HighRes/playButton.png';
+	this.titleImg.src = 'resources/HighRes/title.png';
+	this.buttons.push(new Button(game.sceneManager.goToScene, "The Game", (window.innerWidth/2 - window.innerWidth/10), window.innerHeight/2, window.innerWidth/10, window.innerHeight/20, this.playImg));
+
+}
+
+TitleScene.prototype = new Scene();
+
+TitleScene.prototype.render = function(){
+	ctx.drawImage(game.backImage, 0, 0, window.innerWidth, window.innerHeight);
+	//Game Title 3D text
+
+	ctx.drawImage(this.titleImg, window.innerWidth / 8, window.innerHeight / 8, window.innerWidth / 1.5, window.innerHeight / 10);
+ 	
+	for(var i = 0; i < this.buttons.length; i++)
+	{
+	 	this.buttons[i].draw();
+	}
+}
+
+TitleScene.prototype.input = function(x, y){
+	for(var i = 0; i < this.buttons.length; i++)
+	{
+		this.buttons[i].isClicked(x,y);
+	}
+}
+
+TitleScene.prototype.onTouchStart = function(x,y){
+	for(var i = 0; i < this.buttons.length; i++)
+	{
+		this.buttons[i].isClicked(x,y);
+	}
+}
+
+TitleScene.prototype.onTouchMove = function(x,y){
+}
+
+TitleScene.prototype.onTouchEnd = function(x,y){
+}
